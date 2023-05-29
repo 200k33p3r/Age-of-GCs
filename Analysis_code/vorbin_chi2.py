4d164e9e68ef0921c7362b107ccdf5a244ca09aa
200k33p3r <martinyingjiaqi@outlook.com>
2023-05-29 17:02:27 -0400

    developing resample_fidanka

--- Analysis_code/vorbin_chi2.py
+++ Analysis_code/vorbin_chi2.py
@@ -5,6 +5,7 @@ import pandas as pd
 import os
 from vorbin.voronoi_2d_binning import voronoi_2d_binning
 import subprocess
+from scipy.interpolate import interp1d
 
 class utiles:
 	def check_file(self,path):
@@ -135,7 +136,20 @@ class utiles:
 		y_gen = np.concatenate((y_gen_MS,y_gen_MSTO,y_gen_GB))
 		return x_gen, y_gen
 
+	def find_fit_val(self,arr,x):
+		for idx in range(len(arr)):
+			if x < arr[idx]:
+				return arr[idx], idx
+		return arr[-1], -1
 
+	#fine the empirical cdf from observational datapoints
+	def empirical_cdf(v):
+		v = np.sort(v)
+		len_v = len(v)
+		cdf = np.linspace(0,1,len_v)
+		v,idx = np.unique(v, return_index=True)
+		cdf = cdf[idx]
+		return v, cdf
 
 class chi2(utiles):
 
@@ -337,38 +351,147 @@ class resample(utiles):
 
 #this is similar to resample class but utilizing the fiducial isochrone generated from fidanka
 class resample_fidanka(utiles):
-	def __init__(self, GC_name, start, end, MSTO_cut, GB_cut, write_vorbin=False, Tb_size=30,write_cmd=False, sample_pt=2000000):
+	def read_input(self,obs_path,phot_path,fiducial_path):
+		#read M92 observed data
+		self.obs_data = pd.read_csv(obs_path)
+		#read AS test error
+		self.dps_Ierr = []
+		self.dps_Verr = []
+		self.completeness_V = []
+		self.completeness_I = []
+		for i in range(80):
+			self.dps_Ierr.append(pd.read_csv("{}/Ierr{:02d}s.dat".format(phot_path,i + 1),sep='\s+',skiprows=3,names=['Ierr']))
+			self.dps_Verr.append(pd.read_csv("{}/Verr{:02d}s.dat".format(phot_path,i + 1),sep='\s+',skiprows=3,names=['Verr']))
+			self.completeness_V.append(pd.read_csv("{}/Verr{:02d}s.dat".format(phot_path,i + 1),sep='\s+',skiprows=1,names=["#","Npts","Radius","Mag","Completeness"],nrows=1)['Completeness'].values[0])
+			self.completeness_I.append(pd.read_csv("{}/Ierr{:02d}s.dat".format(phot_path,i + 1),sep='\s+',skiprows=1,names=["#","Npts","Radius","Mag","Completeness"],nrows=1)['Completeness'].values[0])
+		df_V_bound = pd.read_csv("{}/VerrBoundary.dat".format(phot_path),sep='\s+',skiprows=3, names=['Rad', 'Mag', 'Nstar', 'Completness'])
+		self.Rad_bounds = np.sort(np.unique(df_V_bound['Rad'].values))
+		self.V_mag_bounds = np.sort(np.unique(df_V_bound['Mag'].values))
+		df_I_bound = pd.read_csv("{}/IerrBoundary.dat".format(phot_path),sep='\s+',skiprows=3, names=['Rad', 'Mag', 'Nstar', 'Completness'])
+		self.Rad_bounds = np.sort(np.unique(df_I_bound['Rad'].values))
+		self.I_mag_bounds = np.sort(np.unique(df_I_bound['Mag'].values))
+		#read fiducial lines
+		fiducial = pd.read_csv(fiducial_path,names=['vi','v','c5','c95','perp'])
+		self.V_fid = fiducial['v'].values
+		I_fid = fiducial['v'].values - fiducial['vi'].values
+		self.ff = interp1d(self.V_fid, I_fid, bounds_error=False, fill_value='extrapolate')
+
+	#maginalized the completeness from the result of AS test. We assume the radius and magnitude are indepedent
+	def marginalize(self,v):
+		self.completeness_V_marginalzied = []
+		self.completeness_I_marginalzied = []
+		self.completeness_R_marginalized = []
+		x = self.obs_data['x'] - self.OBS_center[0]
+		y = self.obs_data['y'] - self.OBS_center[1]
+		r = np.sqrt(x**2 + y**2)
+		r, r_cdf = self.empirical_cdf(r)
+		i = self.ff(v)
+		v, v_cdf = self.empirical_cdf(v)
+		i = self.ff(v)
+		i, i_cdf = self.empirical_cdf(i)
+		#do Vmag
+		for j in range(len(self.V_mag_bounds)):
+			maginalzied_prob = 0
+			i = 0
+			k = 0
+			idxs = []
+			while i < len(self.Rad_bounds):
+				while k < len(r) - 1:
+					if (r[k+1] > self.Rad_bounds[i]) & (r[k] <= self.Rad_bounds[i]):
+						idxs.append(k+1)
+						i += 1
+						k += 1
+					else:
+						k += 1
+			#in case it runs outside of the bound
+			if len(idxs) < len(self.Rad_bounds):
+				idxs.append(-1)
+			for i in range(len(self.Rad_bounds)):
+				if i==0:
+					maginalzied_prob += self.completeness_V[len(self.Rad_bounds)*j + i]*r_cdf[idxs[i]]
+				else:
+					maginalzied_prob += self.completeness_V[len(self.Rad_bounds)*j + i]*(r_cdf[idxs[i]] - r_cdf[idxs[i-1]])
+			self.completeness_V_marginalzied.append(maginalzied_prob)
+
+		#do Imag
+		for j in range(len(self.I_mag_bounds)):
+			maginalzied_prob = 0
+			i = 0
+			k = 0
+			idxs = []
+			while i < len(self.Rad_bounds):
+				while k < len(r) - 1:
+					if (r[k+1] > self.Rad_bounds[i]) & (r[k] <= self.Rad_bounds[i]):
+						idxs.append(k+1)
+						i += 1
+						k += 1
+					else:
+						k += 1
+			#in case it runs outside of the bound
+			if len(idxs) < len(self.Rad_bounds):
+				idxs.append(-1)
+			for i in range(len(self.Rad_bounds)):
+				if i==0:
+					maginalzied_prob += self.completeness_I[len(self.Rad_bounds)*j + i]*r_cdf[idxs[i]]
+				else:
+					maginalzied_prob += self.completeness_I[len(self.Rad_bounds)*j + i]*(r_cdf[idxs[i]] - r_cdf[idxs[i-1]])
+			self.completeness_I_marginalzied.append(maginalzied_prob)
+		#do r
+
+		
+	#inferencing the real cdf from the observational data with completeness test
+	def cdf_inference(self,v):
+		len_v = len(v)
+		v, cdf = self.empirical_cdf(v)
+		i = self.ff(v)
+		cdf_new = np.zeros(len_v)
+		for i in range(len_v):
+			_, idx = self.find_fit_val(self.V_mag_bounds,v[i])
+			v_comp = self.completeness_V_marginalzied[idx]
+			_, idx = self.find_fit_val(self.I_mag_bounds,i[i])
+			i_comp = self.completeness_I_marginalzied[idx]
+			if i == 0:
+				cdf_new[0] = cdf[0]/(v_comp*i_comp)
+			else:
+				cdf_new[i] = cdf_new[i-1] + (cdf[i] - cdf[i-1])/(v_comp*i_comp)
+		cdf_new /= cdf_new[-1]
+		return v, cdf_new
+	
+	def get_cdf(self,cdf_path):
+		self.marginlize()
+		v, cdf_new = self.cdf_inference(self.obs_data['v'].values)
+
+	def resample(self, Binary_Fraction, sample_pt):
+		1
+
+
+	def __init__(self, GC_name, start, end, Binary_Fraction = 0.02, write_vorbin=False, Tb_size=30,write_cmd=False, sample_pt=4000000):
 		#define boundaris
 		if GC_name == 'M55':
 			obs_vi_max = 0.792
 			obs_vi_min = 0.462
 			obs_v_max = 19.28
 			obs_v_min = 15.296
-		#define distance modulus and reddening ranges
-		if GC_name == 'M55':
-			dm_max = 14.00
-			dm_min = 13.50
-			red_max = 0.15
-			red_min = 0.00
-			dm_num = 51
-			red_num = 16
-		dms = np.linspace(dm_min,dm_max,dm_num)
-		reds = np.linspace(red_min,red_max,red_num)
-		#define boundaris
-		obs_vi_max = 0.791
-		obs_vi_min = 0.473
-		obs_v_max = 19.279
-		obs_v_min = 15.297
 		width_coeff = (obs_v_max - obs_v_min)/(obs_vi_max - obs_vi_min)
+		#correct the difference between obs data and as test
+		if GC_name == 'M55':
+			self.AS_center = [3005.49976,2997.75391]
+			self.OBS_center = [2995.02393,3020.84351]
+			self.V_diff=30.98
+			self.I_diff = self.V_diff-0.737
 		#define all the path for read and write
-		obs_data_path = "C:\\Users\\marti\\Desktop\\school work\\Dartmouth\\GC_ages\\{}\\{}_fitstars_with_bins.dat".format(GC_name,GC_name)
-		photometry_path = "C:\\Users\\marti\\Desktop\\school work\\Dartmouth\\GC_ages\\{}\\inputfiles".format(GC_name)
-		vorbin_path = "C:\\Users\\marti\\Desktop\\school work\\Dartmouth\\GC_ages\\{}\\resample\\vorbin".format(GC_name)
-		chi2_path = "C:\\Users\\marti\\Desktop\\school work\\Dartmouth\\GC_ages\\{}\\resample\\outchi2".format(GC_name)
-		cmd_path = "C:\\Users\\marti\\Desktop\\school work\\Dartmouth\\GC_ages\\{}\\resample\\cmd".format(GC_name)
+		if GC_name == 'M55':
+			resample_path = '/media/sf_share/M55_data/resample'
+			data_path = '/home/mying/Desktop/GC_Ages/Age-of-GCs/M55_data'
+		photometry_path = "{}/inputfiles".format(resample_path)
+		vorbin_path = "{}/vorbin".format(resample_path )
+		chi2_path = "{}/outchi2".format(resample_path)
+		cmd_path = "{}/cmd".format(resample_path )
+		obs_path = "{}/M55_fitstars.dat".format(data_path)
+		fiducial_path = "{}/fiducial_lines.csv".format(data_path)
 		#check those directories exist
-		self.check_file(obs_data_path)
 		self.check_file(photometry_path)
+		self.check_file(fiducial_path)
 		self.check_directories(vorbin_path)
 		self.check_directories(chi2_path)
 		self.check_directories(cmd_path)
@@ -376,10 +499,11 @@ class resample_fidanka(utiles):
 		self.Tb_size = Tb_size
 		self.sample_pt = sample_pt
 		#find both cuts from observational data
-		self.MSTO_cut = MSTO_cut
-		self.GB_cut = GB_cut
-		#read obs data
-		self.read_input(obs_data_path,photometry_path)
+		if GC_name == 'M55':
+			MSTO_cut = 17.0
+			GB_cut = 18.0
+		#read obs data and photometry data
+		self.read_input(obs_path,photometry_path,fiducial_path)
 		#run resample
 		self.chi2 = []
 		for k in range(start, end):
