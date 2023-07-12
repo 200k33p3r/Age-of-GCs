80df9f72bd4bc2461cdf86e79ff7ce7b9a37b2a6
200k33p3r <martinyingjiaqi@outlook.com>
2023-07-12 15:43:22 -0400

    Add method to determine phot error using fiducial isochrone

---
+++ Photometry/phot_fidanka.py
@@ -0,0 +1,79 @@
+#This code is designed to determine the photometric error for HST ACS globular cluster data
+#in case the photometric uncertainty estimated from the artifical star test is not sufficient
+#to explain the width in the CMD.
+#This is likely to happen in GCs which does not suffer from crowding.
+#The result file is an sudo artificial star test result that can be feeded into the photometry code
+
+import numpy as np
+import pandas as pd
+import os
+import shapely.geometry as geom
+import sys
+
+class phot:
+    #read in fitstars and fiducial isochrone generated from the fidanka package
+    def read_input(self, data_path, iso_path):
+        fitstars = pd.read_csv(data_path)
+        iso = pd.read_csvv(iso_path)
+        v_star = fitstars['v'].values
+        i_star = fitstars['i'].values
+        v_iso = iso['v'].values
+        i_iso = iso['v'].values - iso['vi'].values
+        x_star = fitstars['x'].values
+        y_star = fitstars['y'].values
+        return v_star, i_star, v_iso, i_iso, x_star, y_star
+    
+    #Even though the artificial star test is not capable of estimating the photometric
+    #error in certain cases, it is still helpfuly in determine the completeness.
+    #In this case, we adopt the completeness determined from AS test
+    # def read_completeness(self, input_file_path):
+    #     #do v first
+    #     Verr = pd.read_csv("{}/VerrBoundart.dat",skiprows=3, names=['outboundRad','minMagBound','Nstar','Completenss'])
+    #     #do i
+    #     Ierr = pd.read_csv("{}/IerrBoundart.dat",skiprows=3, names=['outboundRad','minMagBound','Nstar','Completenss'])
+
+    
+    def determine_error(self, v_star, i_star, v_iso, i_iso):
+        coords = np.vstack((v_iso,i_iso)).T
+        line = geom.LineString(coords)
+        out_v = np.zeros(len(v_star))
+        out_i = np.zeros(len(i_star))
+        for i in range(len(v_star)):
+            point = geom.Point(np.array([v_star[i], i_star[i]]))
+            point_on_line = line.interpolate(line.project(point))
+            out_v[i] = point_on_line.x
+            out_i[i] = point_on_line.y
+        return out_v, out_i
+    
+    #write the fiducial AS test
+    def write_out(self, fiducial_AS_path, v_star, i_star, out_v, out_i, x_star, y_star):
+        data = np.vstack((x_star, y_star, v_star - self.V_diff, i_star - self.I_diff, x_star, y_star, out_v - self.V_diff, out_i - self.I_diff)).T
+        df = pd.DataFrame(data=data, colums=['InputX','InputY','InputF606W','InputF814W','OutputX','OutputY','OutputF606W','OutputF814W'])
+        df.to_csv(fiducial_AS_path,index=False,sep=' ')
+    
+    def __init__(self,GC_name):
+        if GC_name == 'M55':
+            self.V_diff = 30.98
+            self.I_diff = self.V_diff-0.737
+                #define all the path for read and write
+        if GC_name == 'M55':
+            # repo_path = '/home/mying/Desktop/GC_Ages/Age-of-GCs'
+            # resample_path = '/media/sf_share/{}_data/resample'.format(GC_name)
+            repo_path = '/home/mying/Desktop/Repositories/Age-of-GCs'
+        data_path = "{}/{}_data".format(repo_path, GC_name)
+        photometry_folder = "{}/Photometry".format(repo_path)
+        photometry_path = "{}/{}_inputfiles".format(photometry_folder,GC_name)
+        obs_data_path = "{}/{}_fitstars.dat".format(data_path,GC_name)
+        iso_path = "{}/fiducial_lines.csv".format(data_path)
+        fiducial_AS_path = "{}/{}_fiducial_artstars.dat".format(data_path,GC_name)
+
+        #read files
+        v_star, i_star, v_iso, i_iso, x_star, y_star = self.read_input(obs_data_path, iso_path)
+        #determine error
+        out_v, out_i = self.determine_error(v_star, i_star, v_iso, i_iso)
+        #write output
+        self.write_out(fiducial_AS_path, v_star, i_star, out_v, out_i, x_star, y_star)
+
+#read input and run code
+GC_name = str(sys.argv[1])
+phot(GC_name)
--- Photometry/run_phot_fidanka.sh
+++ Photometry/run_phot_fidanka.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+#this code use fiducial isochrones instead of AS test to determine the photometric error
 
 GC_name=$1
-file_dir="${GC_name}_inputfiles"
-file_temp="${GC_name}_inputfiles_temp"
+file_dir="${GC_name}_fidanka_inputfiles"
+file_temp="${GC_name}_fidanka_inputfiles_temp"
 mkdir ${file_dir}
 mkdir ${file_temp}
+
+#write the result of fiducial isochrone fitting into AS test file format
+python phot_fidanka.py $GC_name
 
 cp define_mags.f90 ${file_temp}
 cp PhotoPropI.f90 ${file_temp}
@@ -18,7 +22,7 @@ gfortran PhotoPropI.f90 define_mags.o -o PhotoPropI
 gfortran -c qsort.f90
 gfortran RadialDensity.f90 qsort.o -o RadialDensity
 
-AS_path=../../${GC_name}_data/${GC_name}_fiducial_artstars.dat
+AS_path=../../${GC_name}_data/${GC_name}artstars.dat
 obs_path=../../${GC_name}_data/${GC_name}_fitstars.dat
 
 AS_stars=$((`wc --lines < $AS_path` - 1))
@@ -35,6 +39,3 @@ cp fit_id.dat ../${file_dir}
 cp Distance.dat ../${file_dir}
 cd ..
 rm -rf ${file_temp}
-
-
-
