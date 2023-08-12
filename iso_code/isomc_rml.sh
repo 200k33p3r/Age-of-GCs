0719317518dbac5c61c0c65d7b1e78b858662e44
200k33p3r <martinyingjiaqi@outlook.com>
2023-08-11 21:26:07 -0400

    Include methods to generate isochrones for individual star fitting

--- iso_code/isomc_rml.sh
+++ iso_code/isomc_rml.sh
@@ -1,15 +1,17 @@
 #! /bin/csh
+#modified from isomc.sh to output only radius, mass and luminosity
+#for fitting individual stars
 #calculate isochrones using *.iso output files 
 #from a red giant branch monte carlo simulation
 
  setenv local  /home/mying/Desktop/iso_code
- setenv track  /data/M55_tracks
- setenv var   /data/M55_var
+ setenv track  /data/NGC3201_data/NGC3201_tracks
+ setenv var   /data/NGC3201_data/NGC3201_var
  setenv run   $local
  setenv out   $local/outiso
 
 #define other variables
- @ feh = 190
+ @ feh = 148
 
 #read from the command line which runs to do
  if ($#argv != 2) then
@@ -54,53 +56,11 @@
 #output file
     ln -s  $out/feh${feh}.$r                     fort.92
 
-    time $run/isomc2
-
-    rm fort.*
-
-# now, replace the low mass points with the FreeEOS runs
-    ln -s $var/varfeh-${feh}.$r                      fort.75
-    ln -s  $out/feh${feh}.$r                     fort.11
-
-    @ tn = 1
-    @ good_track = 0
-    foreach m (0200 0240 0280 0320 0360 0400 0440 0480 0520 0560 0600 0640 0680)
-      @ fn = 20 + $tn
-      @ size=`ls -l iso_m$m.feh${feh}.$r | awk '{print $5}'`
-      #exclude failed models
-      if ($size > 1000) then
-         ln -s  iso_m$m.feh${feh}.$r               fort.$fn
-         @ tn = $tn + 1
-         @ good_track = $good_track + 1
-      endif
-    end
-#update the number of track in input.nml
-    sed -i "9s/.*/  NTRKLOW = ${good_track}/" isomc.nml
-#output file
-    ln -s  $out/feh${feh}l.$r       fort.12
-
-    set ntrklow=`grep NTRKLOW isomc.nml  | awk '{print $3}' `
-    set numage=`grep Numage isomc.nml   | awk '{print $3}' `
-
-
-    time $run/lowmass2 $ntrklow $numage 0.691
-
-    rm fort.*
-    rm iso_m*.feh${feh}.$r
-
-
-# finally, create a file suitable for fake CMD generation
-    ln -s $out/feh${feh}l.$r              fort.11
-# output file
-     ln -s  $out/feh${feh}cmd.$r       fort.12
-
-     time $run/prepiso $numage 0.691
+    time $run/isomc_rml
 
      rm fort.*
+     rm iso_m*.feh${feh}.$r
 
-  
 
  @ r++
 end
-
-
