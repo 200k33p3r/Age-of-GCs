0b3d2d42f2152e5a1ec720999ceea1d6f0fa8e02
200k33p3r <martinyingjiaqi@outlook.com>
2023-08-31 16:50:28 -0400

    Improve performance when calculating chi2

--- Analysis_code/rml_inference.py
+++ Analysis_code/rml_inference.py
@@ -13,31 +13,50 @@ class utile:
     
     def read_candidates(self,star_path):
         dp = pd.read_csv(star_path)
-        return dp
+        N_stars = len(dp)
+        Names = dp['Names'].values
+        Mass_star = dp['M/Ms'].values.reshape(N_stars,1)
+        Mass_star_p_err = dp['+dM/Ms'].values.reshape(N_stars,1)
+        Mass_star_m_err = dp['-dM/Ms'].values.reshape(N_stars,1)
+        Lumi_star = dp['L/Ls'].values.reshape(N_stars,1)
+        Lumi_star_p_err = dp['+dL/Ls'].values.reshape(N_stars,1)
+        Lumi_star_m_err = dp['-dL/Ls'].values.reshape(N_stars,1)
+        Rad_star = dp['R/Rs'].values.reshape(N_stars,1)
+        Rad_star_p_err = dp['+dR/Rs'].values.reshape(N_stars,1)
+        Rad_star_m_err = dp['-dR/Rs'].values.reshape(N_stars,1)
+        return Names, Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err
 
-    def calculate_chi2(self, Mass,Lumi,Rad, candidates):
-        N_stars = len(candidates)
+    def calculate_chi2(self, Mass,Lumi,Rad, Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err):
+        N_stars = len(Mass_star)
         N_eeps = len(Mass)
         chi2 = np.zeros((N_eeps,N_stars))
+        #reshape for boardcasting
+        Mass = Mass.reshape([1,N_eeps])
+        Lumi = Lumi.reshape([1,N_eeps])
+        Rad = Rad.reshape([1,N_eeps])
+        #calculate difference
+        Mass_diff = Mass - Mass_star
+        Lumi_diff = Lumi - Lumi_star
+        Rad_diff = Rad - Rad_star
+        #find whether the difference is greater than 0 or not
+        Mass_TF = Mass_diff > 0
+        Lumi_TF = Lumi_diff > 0
+        Rad_TF = Rad_diff > 0
+        #define chi2
+        Mass_chi2 = np.zeros((N_stars, N_eeps))
+        Rad_chi2 = np.zeros((N_stars, N_eeps))
+        Lumi_chi2 = np.zeros((N_stars, N_eeps))
+        #for each star, evaluate whether the difference is positive or not. Then divide by the corresponding uncertainty
         for i in range(N_stars):
-            star_vals = candidates.iloc[i].values[1:].astype(float)
-            for j in range(N_eeps):
-                #fit mass
-                if Mass[j] >= star_vals[0]:
-                    chi2[j,i] += (Mass[j] - star_vals[0])**2/(star_vals[1]**2)
-                else:
-                    chi2[j,i] += (Mass[j] - star_vals[0])**2/(star_vals[2]**2)
-                #fit luminosity
-                if Lumi[j] >= star_vals[3]:
-                    chi2[j,i] += (Lumi[j] - star_vals[3])**2/(star_vals[4]**2)
-                else:
-                    chi2[j,i] += (Lumi[j] - star_vals[3])**2/(star_vals[5]**2)
-                #fit radius
-                if Rad[j] >= star_vals[6]:
-                    chi2[j,i] += (Rad[j] - star_vals[6])**2/(star_vals[7]**2)
-                else:
-                    chi2[j,i] += (Rad[j] - star_vals[6])**2/(star_vals[8]**2)
-        indi_fit = np.min(chi2,axis=0)
+            Mass_chi2[i] = np.divide(Mass_diff[i],np.where(Mass_TF[i],Mass_star_p_err[i],Mass_star_m_err[i]))
+            Lumi_chi2[i] = np.divide(Lumi_diff[i],np.where(Lumi_TF[i],Lumi_star_p_err[i],Lumi_star_m_err[i]))
+            Rad_chi2[i] = np.divide(Rad_diff[i],np.where(Rad_TF[i],Rad_star_p_err[i],Rad_star_m_err[i]))
+        #square the result
+        Mass_chi2 = np.square(Mass_chi2)
+        Lumi_chi2 = np.square(Lumi_chi2)
+        Rad_chi2 = np.square(Rad_chi2)
+        chi2 = Mass_chi2 + Lumi_chi2 + Rad_chi2
+        indi_fit = np.min(chi2,axis=1)
         minchi2 = np.sum(indi_fit)
         return minchi2, indi_fit.tolist()
 
@@ -46,13 +65,13 @@ class utile:
 
 
 class run(utile):
-    def main(self,iso_path,candidates,mc_num):
+    def main(self,iso_path,Names,Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err,mc_num):
         self.check_file(iso_path)
         iso = open(iso_path, 'r')
         len_file = len(iso.readlines())
         iso.seek(0)
         len_idx = 1
-        head = np.concatenate((np.array(['mc_num','Age','chi2']),candidates['Names'].values))
+        head = np.concatenate((np.array(['mc_num','Age','chi2']),Names))
         data = []
         if len_file < 10:
             raise Exception("Empty file")
@@ -79,7 +98,7 @@ class run(utile):
                 iso.readline()
                 iso.readline()
                 len_idx += 2
-                minchi2, indi_fit = self.calculate_chi2(Mass,Lumi,Rad,candidates)
+                minchi2, indi_fit = self.calculate_chi2(Mass,Lumi,Rad,Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err)
                 data.append([mc_num,int(AGE[:-1]), minchi2] + indi_fit)
         dp = pd.DataFrame(data=data,columns=head)
         return dp
@@ -87,9 +106,9 @@ class run(utile):
     def __init__(self, iso_path, star_path,wrt_path):
         self.check_file(iso_path)
         self.check_file(star_path)
-        candidates = self.read_candidates(star_path)
+        Names, Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err = self.read_candidates(star_path)
         mc_num = int(iso_path[-5:])
-        dp = self.main(iso_path,candidates,mc_num)
+        dp = self.main(iso_path,Names,Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err, mc_num)
         self.writeout(dp,wrt_path)
 
 class simulate(utile):
@@ -162,15 +181,13 @@ class simulate(utile):
 
 class simulate_iso(utile):
     # extract stars from isochrones, add noise from candidates, rerun the analysis
-    def main(self, iso_path, candidates, mc_num, age):
-        num_stars = len(candidates)
+    def main(self, iso_path,Names,Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err, mc_num, age, candidates):
+        num_stars = len(Names)
         self.check_file(iso_path)
         iso = open(iso_path, 'r')
         len_file = len(iso.readlines())
         iso.seek(0)
         len_idx = 1
-        head = np.array(['mc_num','Age','chi2','num_shifts'])
-        data = []
         if len_file < 10:
             raise Exception("Empty file")
         else:
@@ -201,7 +218,7 @@ class simulate_iso(utile):
                     Mass_alter = Mass[pick_idx]
                     Lumi_alter = Lumi[pick_idx]
                     Rad_alter = Rad[pick_idx]
-                    sample_error= pd.DataFrame(columns = ['+dM/Ms','-dM/Ms','+dL/Ls','-dL/Ls','+dR/Rs','-dR/Rs'], data = np.abs(np.random.normal(scale=candidates[['+dM/Ms','-dM/Ms','+dL/Ls','-dL/Ls','+dR/Rs','-dR/Rs']].values)))
+                    sample_error= pd.DataFrame(columns = ['+dM/Ms','-dM/Ms','+dL/Ls','-dL/Ls','+dR/Rs','-dR/Rs'], data = np.abs(np.random.normal(scale=np.concatenate((Mass_star_p_err, Mass_star_m_err, Lumi_star_p_err, Lumi_star_m_err, Rad_star_p_err, Rad_star_m_err),axis=1))))
                     sample_error[['-dM/Ms','-dL/Ls','-dR/Rs']] = -sample_error[['-dM/Ms','-dL/Ls','-dR/Rs']]
                     candidates['M/Ms'] = Mass_alter + np.array([np.random.choice(sample_error[['+dM/Ms','-dM/Ms']].values[i]) for i in range(num_stars)])
                     candidates['R/Rs'] = Rad_alter + np.array([np.random.choice(sample_error[['+dR/Rs','-dR/Rs']].values[i]) for i in range(num_stars)])
@@ -216,8 +233,9 @@ class simulate_iso(utile):
     def __init__(self, iso_path, star_path, wrt_path, age):
         self.check_file(iso_path)
         self.check_file(star_path)
-        candidates = self.read_candidates(star_path)
+        candidates = pd.read_csv(star_path)
+        Names, Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err = self.read_candidates(star_path)
         #assume the uncertainty from the candidates are "correct" and use them directly.
         mc_num = int(iso_path[-5:])
-        dp = self.main(iso_path,candidates,mc_num,age)
+        dp = self.main(iso_path,Names,Mass_star, Mass_star_p_err, Mass_star_m_err, Lumi_star, Lumi_star_p_err, Lumi_star_m_err, Rad_star, Rad_star_p_err, Rad_star_m_err, mc_num, age, candidates)
         self.writeout(dp,wrt_path)
