682731a60047f901127d9b261e997f49858a18ba
200k33p3r <martinyingjiaqi@outlook.com>
2024-08-02 16:07:13 -0400

    Bug fixing

--- DSEP_code/sobol_mcvar.py
+++ DSEP_code/sobol_mcvar.py
@@ -18,9 +18,9 @@ class genetatemcvar:
     
     def write_out(self,outpath, out_df,feh):
         if feh < 0.0:
-            filehead = '/varfeh-' + str(np.int(np.abs(feh*100))) + '.'
+            filehead = '/varfeh-' + "{:03d}".format(int(np.abs(feh*100))) + '.'
         else:
-            filehead = '/varfeh+' + str(np.int(np.abs(feh*100))) + '.'
+            filehead = '/varfeh+' + "{:03d}".format(int(np.abs(feh*100))) + '.'
         for i, mc_num in enumerate(out_df['mc_num'].values):
             # Define the file to write to
             outfile = outpath + filehead + str(mc_num)
@@ -32,7 +32,7 @@ class genetatemcvar:
                 f.write("{:9.6f}     {:30s}\n".format(out_df['CMIXLA'].values[i], 'CMIXLA'))
                 f.write("{:9.6f}     {:30s}\n".format(out_df['FGRY'].values[i], 'FGRY'))
                 f.write("{:9.6f}     {:30s}\n".format(out_df['FGRZ'].values[i], 'FGRZ'))
-                f.write("{:2d}             {:30s}\n".format(out_df['TTAU'].values[i], 'KTTAU'))
+                f.write("{:2d}             {:30s}\n".format(out_df['TTAU'].values[i].astype(int), 'KTTAU'))
                 f.write("{:9.6f}     {:30s}\n".format(out_df['ALPHAE'].values[i], 'ALPHAE'))
                 f.write("{:9.6f}     {:30s}\n".format(out_df['ALPHAC'].values[i], 'ALPHAC'))
                 f.write("{:9.6f}     {:30s}\n".format(out_df['sstandard_1'].values[i], 'SSTANDARD(1) -- PP'))
@@ -52,38 +52,44 @@ class genetatemcvar:
         #read MC variable lists
         MC_var = pd.read_csv('MC_var_ranges.csv')
         self.MC_var = MC_var.copy()
+        #end exclusive
         Param_set_num = mc_end - mc_start
-        sobol_seq = self.generate_sobol(np.floor(np.log2(Param_set_num)))
+        m = np.floor(np.log2(Param_set_num))
+        sobol_seq = self.generate_sobol(m)
         #check for hard limit
         bound_list = MC_var[MC_var['bound'] == True].index.tolist()
         #calculate the lower bound
-        lower_bound = np.norm(lower_bound,loc = MC_var.iloc[bound_list]['var2'].values,scale=MC_var.iloc[bound_list]['var1'].values)
+        lower_bound = norm.cdf(min_bound,loc = MC_var.iloc[bound_list]['var2'].values,scale=MC_var.iloc[bound_list]['var1'].values)
         #compare values and create final mask
         masks = []
         for i in range(len(bound_list)):
             masks.append(sobol_seq[:,bound_list[i]] > lower_bound[i])
         final_mask = np.logical_and.reduce(masks)
+        resample_runs = 0
         while len(sobol_seq[final_mask]) < Param_set_num:
             #resample
-            sobol_seq = self.generate_sobol(np.floor(np.log2(Param_set_num)))
+            if resample_runs > 5:
+                #give up and increase the number of samples
+                m += 1
+            sobol_seq = self.generate_sobol(m)
             masks = []
             for i in range(len(bound_list)):
                 masks.append(sobol_seq[:,bound_list[i]] > lower_bound[i])
             final_mask = np.logical_and.reduce(masks)
-        final_sobol_seq = np.random.choice(sobol_seq[final_mask], size=Param_set_num, replace=False)
+        final_sobol_seq = sobol_seq[final_mask][np.random.choice(np.arange(len(sobol_seq[final_mask])), size=Param_set_num, replace=False)]
         #now we convert the sobol sequence to actual output
         Out_MC_var = np.zeros(np.shape(final_sobol_seq))
         for i in range(len(MC_var)-2):
             if MC_var['dist'][i] == 'Uniform':
-                Out_MC_var[i] = final_sobol_seq[:,i]*MC_var['var1'][i] + MC_var['var2'][i]
+                Out_MC_var[:,i] = final_sobol_seq[:,i]*MC_var['var1'].iloc[i] + MC_var['var2'].iloc[i]
             elif MC_var['dist'][i] == 'Gaussian':
-                Out_MC_var[i] = norm.ppf(final_sobol_seq[:,i])*MC_var['var1'][i]+MC_var['var2'][i]
+                Out_MC_var[:,i] = norm.ppf(final_sobol_seq[:,i])*MC_var['var1'].iloc[i]+MC_var['var2'].iloc[i]
             else:
                 raise Exception('Unspecified distribution')
         #get feh
-        Out_MC_var[-2] = norm.ppf(final_sobol_seq[:,-2])*sigma_feh + feh
+        Out_MC_var[:,-2] = norm.ppf(final_sobol_seq[:,-2])*sigma_feh + feh
         #get afe
-        Out_MC_var[-1] = norm.ppf(final_sobol_seq[:,-2])*sigma_afe + afe
+        Out_MC_var[:,-1] = norm.ppf(final_sobol_seq[:,-2])*sigma_afe + afe
         #for [a/Fe], only have tables in steps of 0.2dex, so need to round to the nearest tabulated value
         for i in range(len(Out_MC_var[-1])):
             if Out_MC_var[-1,i] < -0.10:
@@ -108,16 +114,14 @@ class genetatemcvar:
             else:
                 Out_MC_var[KATTU_idx,i] = 5
         #finally, get the correct Helium abundance
-        z = np.zeros(len(Out_MC_var[-3]))
-        for i in range(len(Out_MC_var[-3])):
-            sumz = gs98getz.gs98getz(Out_MC_var[-2,i], Out_MC_var[-1,i])
-            z[i] = 0.244 + MC_var[MC_var['name']=='dyprim']['var1']*Out_MC_var[-3,i] + MC_var[MC_var['name']=='dyprim']['var2'] + sumz*(MC_var[MC_var['name']=='dydz']['var1']*Out_MC_var[-4,i] + MC_var[MC_var['name']=='dydz']['var2'])
-        Out_MC_var[-4] = z
-        #delete the row-3
-        Out_MC_var = np.delete(Out_MC_var,-3)
+        z = np.zeros(len(Out_MC_var))
+        for i in range(len(Out_MC_var)):
+            sumz = gs98getz.gs98getz(Out_MC_var[i,-2], Out_MC_var[i,-1])
+            z[i] = 0.244 + MC_var[MC_var['name']=='dyprim']['var1'].values*Out_MC_var[i,-3] + MC_var[MC_var['name']=='dyprim']['var2'].values + sumz*(MC_var[MC_var['name']=='dydz']['var1'].values*Out_MC_var[i,-4] + MC_var[MC_var['name']=='dydz']['var2'].values)
+        Out_MC_var = np.hstack((Out_MC_var,z.reshape(len(z),1)))
         #write output as a pandas dataframe
-        col_names = np.concatenate(MC_var['name'].values[:-2],np.array(['He Abundance','FeH','alphafe']))
+        col_names = np.concatenate((MC_var['name'].values,np.array(['FeH','alphafe','He Abundance'])))
         out_df = pd.DataFrame(data=Out_MC_var, columns =col_names)
         out_df['mc_num'] = np.arange(mc_start,mc_end)
         #write outfile
-        self.write_out(outpath, out_df,feh)
+        self.write_out(outpath, out_df, feh)
